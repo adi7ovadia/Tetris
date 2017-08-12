820285df3c727f562dab9a32d662db35340db1fb
adi7ovadia <30752845+adi7ovadia@users.noreply.github.com>
2017-08-12 10:40:40 +0300

    Add files via upload

--- src/tests/tests.java
+++ src/tests/tests.java
@@ -7,10 +7,25 @@ import Tetris.Server;
 public class tests 
 {
 	private String userName = "ADI";
+	private int score = 10 ;
 	
     @Test
-    public void testIsExist(){
+    public void testIsExist()
+    {
        Server.checkUser(userName);
     }
+    
+    @Test
+    public void testClientBiggerThenFive()
+    {
+       Server.checkNumberClientBiggerThenFive();
+    }
+    
+    @Test
+    public void testInsertIntoDB()
+    {
+       Server.writeGameDB(userName, score);
+    }
+    
    
 }
